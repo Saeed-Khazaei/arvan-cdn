064bf5ef1d791d53d952dd0af2320429495ed2e8
Saeed-Khazaei <saeed@bookingvila.com>
2022-09-07 23:44:10 +0430

    add get all articles endpint

--- utils/endpoints.ts
+++ utils/endpoints.ts
@@ -1,9 +1,15 @@
 import { api_url, next_url } from "./constants";
 
 // External server endpoints
+// User
 export const postRegisterUser = `${api_url}/users`;
 export const postLoginUser = `${api_url}/users/login`;
+// Articles
+export const getAllArticlesUrl = `${api_url}/articles`;
 
 // Next server endpoints
+// User
 export const postRegisterUserNextApi = `${next_url}/api/user/register`;
 export const postLoginUserNextApi = `${next_url}/api/user/login`;
+// Articles
+export const getAllArticlesNextApi = `${next_url}/api/articles/all`;
