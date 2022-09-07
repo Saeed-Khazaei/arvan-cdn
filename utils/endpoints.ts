4e6cfa5905678527ff2b66f046e884b27abb9205
Saeed-Khazaei <saeed@bookingvila.com>
2022-09-07 09:44:39 +0430

    add endpoints file

---
+++ utils/endpoints.ts
@@ -0,0 +1,7 @@
+import { api_url, next_url } from "./constants";
+
+// External server endpoints
+export const postLoginUser = `${api_url}/user/login`;
+
+// Next server endpoints
+export const postLoginUserNextApi = `${next_url}/api/`;
