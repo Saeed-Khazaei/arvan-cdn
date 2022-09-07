e067c099fbb047c6aa589089770de906c54558f8
Saeed-Khazaei <saeed@bookingvila.com>
2022-09-07 22:25:06 +0430

    add user register endpoint

--- utils/endpoints.ts
+++ utils/endpoints.ts
@@ -1,7 +1,9 @@
 import { api_url, next_url } from "./constants";
 
 // External server endpoints
-export const postLoginUser = `${api_url}/user/login`;
+export const postRegisterUser = `${api_url}/users`;
+export const postLoginUser = `${api_url}/users/login`;
 
 // Next server endpoints
-export const postLoginUserNextApi = `${next_url}/api/`;
+export const postRegisterUserNextApi = `${next_url}/api/user/register`;
+export const postLoginUserNextApi = `${next_url}/api/user/login`;
